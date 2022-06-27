02a0dcb01ecfc8235d70dd7369379d3a1201b1c9
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-27 17:04:10 -0300

    basemodel edit

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,12 @@ class BaseModel:
     
     def __init__(self, *arg, **kwars):
         self.id = str(uuid.uuid4())
-        self.created_ad = datetime.datetime.now()
-        self.updated_ad = datetime.datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
     
     def __str__(self):
         """ def str """
-        print(f"[BaseModel] ({self.id}) {self.__dict__}")
+        return(f"[BaseModel] ({self.id}) {self.__dict__}")
         
     def save(self):
-        
-        
-    print(strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        self.updated_at = datetime.datetime.now()
