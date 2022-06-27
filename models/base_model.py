a90a9c17f9c0ba8e3c2200684eea1de5e8ca60bd
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-27 16:38:39 -0300

    base

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,24 @@
 
 
 import uuid
+import datetime
+import sys
 
 
 class BaseModel:
     
     """ class BaseModel that defines all common attributes/methods for other classes """
     
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *arg, **kwars):
         self.id = str(uuid.uuid4())
-        
-        
-    print(date_now.strftime("%A %d %B %Y at %H:%M:%S")
-          
+        self.created_ad = datetime.datetime.now()
+        self.updated_ad = datetime.datetime.now()
+    
     def __str__(self):
         """ def str """
         print(f"[BaseModel] ({self.id}) {self.__dict__}")
+        
+    def save(self):
+        
+        
+    print(strftime("%Y-%m-%dT%H:%M:%S.%f"))
