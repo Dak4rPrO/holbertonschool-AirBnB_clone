87ff2195f49f5a6638b04842486f8795e2a57a88
Dak4rPrO <4382@holbertonschool.com>
2022-06-28 00:05:54 -0300

    arreglos

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,17 @@ class BaseModel:
     """ class BaseModel that defines all common attributes/methods for other classes """
     
     def __init__(self, *arg, **kwars):
+        """def init"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
     
     def __str__(self):
         """ def str """
-        return(f"[BaseModel] ({self.id}) {self.__dict__}")
+        return(f"['__class__'] ({self.id}) {self.__dict__}")
         
     def save(self):
+        """ def save """
         self.updated_at = datetime.now()
         models.storage.save()
 
@@ -29,20 +31,15 @@ class BaseModel:
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         self.__dict__['__class__'] = self.__class__.__name__
-<<<<<<< HEAD
-        return __dict__
+
+        return self.__dict__
 
     def __init__(self, *args, **kwargs):
         """method to generate a dictionary of an istance"""
-        if **kwargs:
+        if kwargs is not None and len(kwargs) != 0:
             for name, value in kwargs.items():
-                if name == [__'class'__]:
+                if name == ['__class__']:
                     pass
-                else
-                    ['id'] in kwargs.items():
-
-
-
-=======
+                else:
+                    ['id'] in kwargs.items()
         return self.__dict__
->>>>>>> e800a5b4d89221f30ed0550cb71f05886bf28e6d
