22650b2790f41626843fc2ddb8072dd65c137ed5
AgustinGallotti <Agustingallotti23@gmail.com>
2022-06-28 20:43:58 -0700

    test for save reload model and user for task 8

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+command interpreter
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+	"""class of prompt"""
+
+	def do_quit(self, arg):
+		"""quit the shell"""
+		return 1
+
+	def do_EOF(self, arg):
+		"""quit the shell"""
+		print()
+		return 1
+
+	def emptyline(self):
+		"""don't do anything if there's an empty line"""
+		return 0
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
