01cfb834971105428ae31ff7d9b0bb9c1fafe701
AgustinGallotti <Agustingallotti23@gmail.com>
2022-06-28 20:41:03 -0700

    base model with var stored and init update

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Create FileStorage instance for application
+"""
+from models.engine.file_storage import FileStorage 
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 from datetime import datetime
 import uuid
 import sys
+import models
 """from models import storage"""
 
 
@@ -32,7 +33,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            """storage.new(self)"""
+            models.storage.new(self)
 
     def __str__(self):
         """ def str """
