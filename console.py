ba873ccabbf4c6acaae6285ffeaf04e68e05de94
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-30 15:51:44 -0300

    8 y 9

--- console.py
+++ console.py
@@ -3,7 +3,14 @@
 command interpreter
 """
 import cmd
-
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
 	"""class of prompt"""
@@ -20,7 +27,6 @@ class HBNBCommand(cmd.Cmd):
 	def emptyline(self):
 		"""don't do anything if there's an empty line"""
 		return 0
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" public class user """
+
+class User:
+    email = str("")
+    password = str("")
+    first_name = str("")
+    last_name = str("")
