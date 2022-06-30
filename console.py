9040f2ed0b36573ea7dee0b75da057b7590ba71c
Dak4rPrO <4382@holbertonschool.com>
2022-06-30 13:49:35 -0700

    pycode de console

--- console.py
+++ console.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-command interpreter
-"""
-"""from models import storage"""
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -12,21 +8,26 @@ from models.state import State
 from models.user import User
 import cmd
 
+"""command interpreter"""
+"""from models import storage"""
+
+
 class HBNBCommand(cmd.Cmd):
-	"""class of prompt"""
-	prompt = '(hbnb) '
+    """class of prompt"""
+    prompt = '(hbnb) '
 
-	def do_quit(self, arg):
-		"""quit the shell"""
-		return True
+    def do_quit(self, arg):
+        """quit the shell"""
+        return True
 
-	def do_EOF(self, arg):
-		"""quit the shell"""
-		return True
+    def do_EOF(self, arg):
+        """quit the shell"""
+        return True
 
-	def emptyline(self):
-		"""don't do anything if there's an empty line"""
-		return 0
+    def emptyline(self):
+        """don't do anything if there's an empty line"""
+        return 0
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
