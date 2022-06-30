8269ba6109e77c3f62a9c8cb9829f4504eace85e
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-30 16:51:20 -0300

    varios cambios para que funcione la consola

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,21 +2,15 @@
 """define the storage"""
 
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-class FileStorage(BaseModel):
+class FileStorage():
     """class that stores objects in JSON strings
     file_path (str): path of the JSON file
     objects (dict): stores all objects by class"""
 
     __file_path = 'file.json'
+    __objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
@@ -30,19 +24,13 @@ class FileStorage(BaseModel):
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
-        save_dict = {name: value.to_dict() for name,
-                    value in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as f:
-            json.dump(save_dict, f)
+            json.dump(self.__objects, f, default = str)
 
     def reload(self):
         """deserialize the JSON file to __objects"""
         try:
             with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                FileStorage.__objects = {}
-                for name, value in data.items():
-                    obj = BaseModel(**value)
-                    FileStorage.__objects[name] = obj
-        except FileNotFoundError:
+                FileStorage.__objects = json.load(f)
+        except Exception:
            return
