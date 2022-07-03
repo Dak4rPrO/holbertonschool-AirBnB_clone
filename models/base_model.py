2cfe8a09394aa322d89dc85ddd20e226bc472542
Dak4rPrO <4382@holbertonschool.com>
2022-07-03 12:38:48 -0300

    probando 3

--- models/base_model.py
+++ models/base_model.py
@@ -29,17 +29,19 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
     def __str__(self):
         """ def str """
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
+    @classmethod
     def save(self):
         """ def save """
-        storage.save()
         self.updated_at = datetime.now()
+        storage.save()
+        
 
     def to_dict(self):
         """return a dictionary containning all keys/values of dict"""
