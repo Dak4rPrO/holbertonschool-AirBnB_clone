a21936f185ec28292d053ea04fad0fdff4491f97
Dak4rPrO <4382@holbertonschool.com>
2022-07-03 12:51:09 -0300

    boludes en basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
